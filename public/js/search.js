dbaf86c364940dc94f0aa381e9a613bb62fcb67c
Nimo Beeren <nimobeeren@gmail.com>
2017-03-16 09:04:14 +0100

    Added comments and improved code readability

--- public/js/search.js
+++ public/js/search.js
@@ -13,6 +13,7 @@ const evaluateTitle = (p, q) => q.split(" ").reduce((s, w) => s + (p.title.match
 const evaluateHeadings = (p, q) => q.split(" ").reduce((s, w) => s + p.headings.reduce((s, c) => s + (c.match(new RegExp(w, "gi")) || []).length, 0), 0);
 const evaluateContent = (p, q) => (p.contents.match(new RegExp(q, "gi")) || []).length;
 
+// Retrieve page database
 openUrl("GET", "api/cache", {
         callback: res => {
             try {
@@ -27,28 +28,47 @@ openUrl("GET", "api/cache", {
     }
 );
 
+// Search the pages database, sorting results by relevance
 function search(query) {
+    // Check if database is empty
     if (pages.length == 0) {
+        // Give feedback when searching on an empty database
         return log("Search database has not been updated");
     } else {
+        // Store starting time
         let start = new Date().getTime();
 
         let search = pages.sort((a, b) => {
-            let aTitle = evaluateTitle(a, query), bTitle = evaluateTitle(b, query);
+            // Count occurrences of query in title of pages
+            let aTitle = evaluateTitle(a, query);
+            let bTitle = evaluateTitle(b, query);
 
+            // If title count is equal, look at the headings
             if (aTitle == bTitle) {
-                let aHeadings = evaluateHeadings(a, query), bHeadings = evaluateHeadings(b, query);
+                // Count occurrences of query in headings of pages
+                let aHeadings = evaluateHeadings(a, query);
+                let bHeadings = evaluateHeadings(b, query);
 
+                // If heading count is equal, look at the contents
                 if (aHeadings == bHeadings) {
-                    return evaluateContent(b, query) - evaluateContent(a, query);
+                    // Count occurrences of query in contents of pages
+                    let aContent = evaluateContent(a, query);
+                    let bContent = evaluateContent(b, query);
+
+                    // Decide order based on content occurrences
+                    return bContent - aContent;
                 }
+                // Decide order based on heading occurrences
                 return bHeadings - aHeadings;
             }
+            // Decide order based on title occurrences
             return bTitle - aTitle;
         }).map(x => syntaxHighlight(JSON.stringify(x))).slice(0, 9);
 
+        // Calculate running time
         let run = new Date().getTime() - start;
 
+        // Return running time
         return "Took: " + run + "ms <br>" + search;
     }
 }
