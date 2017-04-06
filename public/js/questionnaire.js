3fad57953923137d140cfb2c14702c8f3d38639d
kedar <k.r.kasarekar@student.tue.nl>
2017-04-06 21:20:54 +0200

    Last button link added questionnaire

--- public/js/questionnaire.js
+++ public/js/questionnaire.js
@@ -2,3 +2,4 @@ id('cm').onclick = () => window.location.href = '/program?profile=cm';
 id('ng').onclick = () => window.location.href = '/program?profile=ng';
 id('em').onclick = () => window.location.href = '/program?profile=em';
 id('nt').onclick = () => window.location.href = '/program?profile=nt';
+id('none').onclick = () => window.location.href = '/program';
